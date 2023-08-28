6570e504485f8b79ffd49dbcc6f709b9c2a92ab7
victorhanert <vict660m@stud.kea.dk>
2023-08-28 11:11:57 +0200

    first

---
+++ src/main/java/dk/kea/azuretraining/AzureTrainingApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.azuretraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/azuretraining/AzureTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.azuretraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
